7403a2a3ad2a43e3b1b8601119037f839143780d
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-15 22:12:16 +0530

    Created a script to print Conversations

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This is a script about a conversation and how we get the need of variables
+
+echo "Bujji: Hi Nanna"
+echo "Nanna: Hi bujji How are You!"
+echo "Bujji: I am fine dear just was missing u a lot!"
+echo "Nanna: Me too dear I love U"
