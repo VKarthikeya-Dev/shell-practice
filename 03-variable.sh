960c890d6a5d23ef5a7ebc287e1612b64d6d03cc
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-15 22:17:09 +0530

    Variables

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#about varaibles the usual way
+
+PERSON1=Bujji
+PERSON2=Nanna
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1 How are You!"
+echo "$PERSON1: I am fine dear just was missing u a lot!"
+echo "$PERSON2: Me too dear I love U"
