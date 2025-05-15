8e2dbe00e091d907b54aae359b28a3d35e1a5b88
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-15 22:19:32 +0530

    Variable in arguments

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#sending varaibles as arguments in the UI
+
+PERSON1=$2
+PERSON2=$1
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1 How are You!"
+echo "$PERSON1: I am fine dear just was missing u a lot!"
+echo "$PERSON2: Me too dear I love U"
