3d04f9d565d1b5bced223eef24743cd33a5548a0
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-16 20:20:04 +0530

    Special Variables updated

--- 08-special.sh
+++ 08-special.sh
@@ -3,7 +3,8 @@
 echo "Pass all variables to the script $@"
 echo "Number of variables passed to the script $#"
 echo "The Current directory of the script $PWD"
-echo "The Home directory of the current user running the script $USER"
+echo "The Home directory of the current user running the script $HOME"
+echo "The current user running the script $USER"
 echo "The PID of the script executed $$"
 sleep 10 &
 echo "The PID of the last command in background $!"
