11508f64bbbc9263923f1c82508d967d2af3e097
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-22 06:36:22 +0530

    colours

--- 11-colours.sh
+++ 11-colours.sh
@@ -2,5 +2,5 @@
 
 #colours
 
-echo -e "\e[31m Hello world" # -e is for enabling the colours and the syntax is \e[<style><colour>m
+echo -e "\e[31m Hello world \e[0m" # -e is for enabling the colours and the syntax is \e[<style><colour>m
 echo -e "Hello no colour"
