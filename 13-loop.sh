ec9ea8d14a072ae061a00298ed60206bcbc35e86
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-22 17:06:11 +0530

    Loop

---
+++ 13-loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#for loop syntax: for <var> <value1 value 2 ..value n>
+#                  do
+#                    command
+#                  done
+
+for i in 1..100
+do 
+    echo $i
+done
