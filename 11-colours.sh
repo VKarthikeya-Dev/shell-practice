81e72bee1499eef1b45c7ac4029b283eb5df3cc1
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-22 06:41:34 +0530

    colours

--- 11-colours.sh
+++ 11-colours.sh
@@ -2,5 +2,49 @@
 
 #colours
 
-echo -e "\e[31m Hello world \e[0m" # -e is for enabling the colours and the syntax is \e[<style><colour>m
-echo -e "Hello no colour"
+#echo -e "\e[31m Hello world \e[0m" # -e is for enabling the colours and the syntax is \e[<style><colour>m
+#echo -e "Hello no colour"
+
+#!/bin/bash
+
+#conditional statements in bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "$R The user doesnt have root access $N"
+    exit 1
+else
+    echo "$G The user has root axit can procceed for installation $N"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$G $2 installation is a success $N"
+    else
+        echo "$R Failure in installing $2 $N"
+        exit 1
+    fi
+}
+
+SERVICE=$1
+dnf list installed $SERVICE
+if [ $? -ne 0 ]
+then 
+    dnf install $SERVICE -y
+    VALIDATE $? $SERVICE
+else
+    echo "$Y Alrady installed nth to do $N"
+fi
+
+
+
+
