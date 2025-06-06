5efc408af86dfe4a795d741bd692450352555ae5
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-06-06 23:44:29 +0530

    Hello world

---
+++ delete_logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Source_Dir="/home/ec2-user/app-logs"
+Files_Delete=$(find $Source_Dir -name"*.log" -mtime +14)
+
+while IFS= read -r filepath
+do 
+rm -rf $filepath
+done <<<$Files_Delete
